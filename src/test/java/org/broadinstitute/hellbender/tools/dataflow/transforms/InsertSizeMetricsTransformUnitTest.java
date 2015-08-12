76ad103ebd114c815de16787da2f31f090d2dc8b
Louis Bergelson <louisb@broadinstitute.org>
2015-08-12 14:25:12 -0400

    fixup

--- src/test/java/org/broadinstitute/hellbender/tools/dataflow/transforms/InsertSizeMetricsTransformUnitTest.java
+++ src/test/java/org/broadinstitute/hellbender/tools/dataflow/transforms/InsertSizeMetricsTransformUnitTest.java
@@ -5,14 +5,11 @@ import com.google.cloud.dataflow.sdk.coders.SerializableCoder;
 import com.google.cloud.dataflow.sdk.runners.DirectPipelineRunner;
 import com.google.cloud.dataflow.sdk.transforms.Combine;
 import com.google.cloud.dataflow.sdk.transforms.Create;
-import com.google.cloud.dataflow.sdk.transforms.DoFn;
 import com.google.cloud.dataflow.sdk.transforms.View;
 import com.google.cloud.dataflow.sdk.util.SerializableUtils;
 import com.google.cloud.dataflow.sdk.values.KV;
 import com.google.cloud.dataflow.sdk.values.PCollection;
 import com.google.cloud.dataflow.sdk.values.PCollectionView;
-import com.google.common.collect.ImmutableList;
-import com.google.common.collect.Sets;
 import htsjdk.samtools.metrics.Header;
 import htsjdk.samtools.metrics.MetricsFile;
 import htsjdk.samtools.metrics.StringHeader;
@@ -23,9 +20,8 @@ import org.broadinstitute.hellbender.engine.dataflow.datasources.ReadsDataflowSo
 import org.broadinstitute.hellbender.exceptions.GATKException;
 import org.broadinstitute.hellbender.metrics.MetricAccumulationLevel;
 import org.broadinstitute.hellbender.tools.dataflow.transforms.metrics.HistogramCombinerDataflow;
-import org.broadinstitute.hellbender.tools.dataflow.transforms.metrics.MetricsFileDataflow;
 import org.broadinstitute.hellbender.tools.dataflow.transforms.metrics.HistogramDataflow;
-import org.broadinstitute.hellbender.tools.dataflow.transforms.metrics.insertsize.CombineInsertSizeMetricsFiles;
+import org.broadinstitute.hellbender.tools.dataflow.transforms.metrics.MetricsFileDataflow;
 import org.broadinstitute.hellbender.tools.dataflow.transforms.metrics.insertsize.InsertSizeMetricsDataflowTransform;
 import org.broadinstitute.hellbender.tools.picard.analysis.CollectInsertSizeMetrics;
 import org.broadinstitute.hellbender.tools.picard.analysis.InsertSizeMetrics;
@@ -140,40 +136,13 @@ public final class InsertSizeMetricsTransformUnitTest{
     @Test
     public void testCollectInsertSizeMetric() throws IOException {
         final CollectInsertSizeMetrics collectInsertSizeMetrics = new CollectInsertSizeMetrics();
-        final File bam = new File(BaseTest.publicTestDir, SMALL_BAM);
         collectInsertSizeMetrics.METRIC_ACCUMULATION_LEVEL = EnumSet.of(MetricAccumulationLevel.READ_GROUP);
         collectInsertSizeMetrics.HISTOGRAM_FILE = BaseTest.createTempFile("histogram","pdf");
         collectInsertSizeMetrics.PRODUCE_PLOT = false;
-        collectInsertSizeMetrics.INPUT = bam;
-        final File expectedMetricsFile = BaseTest.createTempFile("expectedMetric", "insertSizeMetric");
-        collectInsertSizeMetrics.OUTPUT = expectedMetricsFile;
+        collectInsertSizeMetrics.INPUT = new File(BaseTest.publicTestDir, SMALL_BAM);
+        collectInsertSizeMetrics.OUTPUT = BaseTest.createTempFile("expectedMetric", "insertSizeMetric");
         collectInsertSizeMetrics.runTool();
-
     }
-
-    @Test
-    public void testBadCase() throws IOException {
-        final CollectInsertSizeMetrics collectInsertSizeMetrics = new CollectInsertSizeMetrics();
-        final File bam = new File(BaseTest.publicTestDir, SMALL_BAM);
-
-        final Pipeline p = GATKTestPipeline.create();
-        DataflowUtils.registerGATKCoders(p);
-
-        ReadsDataflowSource source = new ReadsDataflowSource(bam.getAbsolutePath(), p);
-        PCollection<GATKRead> preads = source.getReadPCollection();
-
-        final List<Header> defaultHeaders = Arrays.asList(new StringHeader("Time stamp"), new StringHeader("--someString"));
-        final PCollectionView<List<Header>> metricHeaders = p.apply(Create.of(defaultHeaders).withCoder(SerializableCoder.of(Header.class))).apply(View.asList());
-
-
-        final InsertSizeMetricsDataflowTransform.Arguments args = new InsertSizeMetricsDataflowTransform.Arguments();
-        args.METRIC_ACCUMULATION_LEVEL = EnumSet.of(MetricAccumulationLevel.READ_GROUP);
-        InsertSizeMetricsDataflowTransform transform = new InsertSizeMetricsDataflowTransform(args, source.getHeaderView(), metricHeaders);
-
-        PCollection<MetricsFileDataflow<InsertSizeMetrics,Integer>> presult = preads.apply(transform);
-        DirectPipelineRunner.EvaluationResults result = (DirectPipelineRunner.EvaluationResults)p.run();
-    }
-
 
     @Test
     public void testHistogrammer(){
@@ -198,88 +167,6 @@ public final class InsertSizeMetricsTransformUnitTest{
             throw new GATKException("there were different numbers of keys and values");
         }
         return kvs;
-    }
-
-    private HistogramDataflow<Integer> createDummyHistogram() {
-        HistogramDataflow<Integer> h1= new HistogramDataflow<>();
-        h1.addInput(10);
-        h1.addInput(20);
-        h1.addInput(10);
-        return h1;
-    }
-
-    @Test
-    public void testCombineMetricsFilePTransform(){
-        final Pipeline p = GATKTestPipeline.create();
-        DataflowUtils.registerGATKCoders(p);
-
-        MetricsFileDataflow<InsertSizeMetrics,Integer> mf1 = new MetricsFileDataflow<>();
-        mf1.addMetric(new InsertSizeMetrics());
-        mf1.addHeader(new StringHeader("header1"));
-        mf1.addHistogram(createDummyHistogram());
-
-        MetricsFileDataflow<InsertSizeMetrics,Integer> mf2 = new MetricsFileDataflow<>();
-        mf2.addMetric(new InsertSizeMetrics());
-        mf2.addHeader(new StringHeader("header2"));
-        mf2.addHistogram(createDummyHistogram());
-
-        MetricsFileDataflow<InsertSizeMetrics,Integer> mf3 = new MetricsFileDataflow<>();
-        mf3.addMetric(new InsertSizeMetrics());
-        mf3.addHeader(new StringHeader("header3"));
-
-
-        PCollection<MetricsFileDataflow<InsertSizeMetrics, Integer>> files = p.apply(Create.of(ImmutableList.of(mf1, mf2, mf3)));
-        PCollection<MetricsFileDataflow<InsertSizeMetrics, Integer>> combined = files.apply(Combine.globally(new CombineInsertSizeMetricsFiles()));
-        DirectPipelineRunner.EvaluationResults results =  (DirectPipelineRunner.EvaluationResults)p.run();
-        MetricsFileDataflow<InsertSizeMetrics, Integer> metricsFile = results.getPCollection(combined).get(0);
-
-        Assert.assertEquals(Sets.newHashSet(metricsFile.getHeaders()), Sets.newHashSet(new StringHeader("header1"), new StringHeader("header2"), new StringHeader("header3")));
-        Assert.assertEquals(metricsFile.getAllHistograms().size(), 2);
-    }
-
-
-    @Test
-    public void testCombineMetricsFiles(){
-        Combine.CombineFn<MetricsFileDataflow<InsertSizeMetrics,Integer>,
-                MetricsFileDataflow<InsertSizeMetrics,Integer>,
-                MetricsFileDataflow<InsertSizeMetrics,Integer>> combiner =
-                new CombineInsertSizeMetricsFiles();
-
-        MetricsFileDataflow<InsertSizeMetrics, Integer> mf1 = getInsertSizeMetricsIntegerMetricsFileDataflow();
-
-        MetricsFileDataflow<InsertSizeMetrics,Integer> mf2 = new MetricsFileDataflow<>();
-        mf2.addMetric(new InsertSizeMetrics());
-        mf2.addHeader(new StringHeader("header2"));
-
-        MetricsFileDataflow<InsertSizeMetrics,Integer> mf3 = new MetricsFileDataflow<>();
-        mf3.addMetric(new InsertSizeMetrics());
-        mf3.addHeader(new StringHeader("header3"));
-
-        @SuppressWarnings("unchecked")
-        MetricsFileDataflow<InsertSizeMetrics, Integer> combined = combiner.apply(ImmutableList.of(mf1, mf2, mf3));
-
-        Assert.assertEquals(combined.getMetrics().size(), 3);
-        Assert.assertEquals(combined.getAllHistograms().size(), 0);
-        Assert.assertEquals(combined.getHeaders().size(), 3);
-
-    }
-
-    private static MetricsFileDataflow<InsertSizeMetrics, Integer> getInsertSizeMetricsIntegerMetricsFileDataflow() {
-        MetricsFileDataflow<InsertSizeMetrics,Integer> mf1 = new MetricsFileDataflow<>();
-        mf1.addMetric(new InsertSizeMetrics());
-        mf1.addHeader(new StringHeader("header1"));
-        return mf1;
-    }
-
-    public static class PrintLn<T> extends DoFn<T,String>{
-        public static final long serialVersionUID = 1l;
-
-        @Override
-        public void processElement(ProcessContext c) throws Exception {
-            String str = c.element().toString();
-            System.out.println(str);
-            c.output(str);
-        }
     }
 
     @Test
@@ -316,7 +203,6 @@ public final class InsertSizeMetricsTransformUnitTest{
 //        Assert.assertEquals(result.getHeaders().size(), expected.getHeaders().size(), "Incorrect number of headers: " +
 //                "expected " + expected.getHeaders().size() + " found " + result.getHeaders().size() +".\n" + "Result:\n" +
 //                result.toString() + "\n Expected:\n" + expected.toString());
-
     }
 
     private List<Histogram<Integer>> getSortedHistograms(MetricsFile<InsertSizeMetrics, Integer> result) {
